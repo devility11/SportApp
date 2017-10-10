608ca6cd4221c853f9cc0702aa79320805c1da0d
Norbert Czirjak <czirjak.norbert@gmail.com>
2017-10-10 23:29:36 +0200

    table and cell added

---
+++ SportApp/View/MainCell.swift
@@ -0,0 +1,32 @@
+//
+//  MainCell.swift
+//  SportApp
+//
+//  Created by Norbert Czirjak on 2017. 10. 10..
+//  Copyright © 2017. Norbert Czirjak. All rights reserved.
+//
+
+import UIKit
+
+class MainCell: UITableViewCell {
+    
+    @IBOutlet weak var homeLbl: UILabel!
+    @IBOutlet weak var awayLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func updateUI(fixtures: GetFixtureData){
+        self.homeLbl.text = fixtures.homeTeam
+        self.awayLbl.text = fixtures.awayTeam
+    }
+
+}
--- SportApp/ViewController.swift
+++ SportApp/ViewController.swift
@@ -10,37 +10,77 @@ import UIKit
 import Alamofire
 import SwiftyJSON
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    var fixturesData = [GetFixtureData]()
+    let fixtures = GetFixtureData()
+    
+    @IBOutlet weak var mainTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.mainTableView.delegate = self
+        self.mainTableView.dataSource = self
+        
         // Do any additional setup after loading the view, typically from a nib.
         let serviceReq = ServiceRequests()
         
         serviceReq.getData(url: socceramaURL+"fixtures/between/2017-10-01/2017-10-09"+socceramaAPI) { response in
             
             self.updateSportData(json: response)
+            
+            print("a fixturesek erteke belul start")
+            print(self.fixturesData)
+            print("a fixturesek erteke belul kivul")
+            
         }
-        
         
     }
     
     
+    //MARK: - Fixtures Data update
+    /***************************************************************/
     
     func updateSportData(json: JSON){
         
         
         for item in json["data"].arrayValue {
+            fixtures.awayTeam = item["visitorteam_id"].stringValue
+            fixtures.homeTeam = item["localteam_id"].stringValue
+            fixtures.date = item["time"]["starting_at"]["date"].stringValue
             print("_____________ START _______________")
             print(item["id"].stringValue)
-            print(item)
+            print(item["visitorteam_id"].stringValue)
+            print(item["localteam_id"].stringValue)
+            //print(item)
             print("_____________ END _________________")
+            fixturesData.append(fixtures)
+            self.mainTableView.reloadData()
         }
         
     }
 
+    //MARK: - TableView Settings
+    /***************************************************************/
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("a countban")
+        print(self.fixturesData.count)
+        return self.fixturesData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if let cell = mainTableView.dequeueReusableCell(withIdentifier: "MainCell", for: indexPath) as? MainCell {
+            print(fixturesData[indexPath.row].awayTeam)
+            print(indexPath.row)
+            let fixtures = fixturesData[indexPath.row]
+            cell.updateUI(fixtures: fixtures)
+            return cell
+        }else {
+            return UITableViewCell()
+        }
+    }
 
     
 

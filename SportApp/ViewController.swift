ff08112c637f7994dd36da1bc931faf251a7a8ce
Norbert Czirjak <czirjak.norbert@gmail.com>
2017-10-11 22:23:26 +0200

    array fixed

--- SportApp/ViewController.swift
+++ SportApp/ViewController.swift
@@ -13,8 +13,8 @@ import SwiftyJSON
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var fixturesData = [GetFixtureData]()
-    let fixtures = GetFixtureData()
     
+
     @IBOutlet weak var mainTableView: UITableView!
     
     override func viewDidLoad() {
@@ -25,16 +25,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         // Do any additional setup after loading the view, typically from a nib.
         let serviceReq = ServiceRequests()
         
-        serviceReq.getData(url: socceramaURL+"fixtures/between/2017-10-01/2017-10-09"+socceramaAPI) { response in
-            
+        serviceReq.getData(url: socceramaURL+"fixtures/date/2017-10-09"+socceramaAPI+"&include=localTeam,visitorTeam,odds") { response in
             self.updateSportData(json: response)
-            
-            print("a fixturesek erteke belul start")
-            print(self.fixturesData)
-            print("a fixturesek erteke belul kivul")
-            
         }
-        
     }
     
     
@@ -42,30 +35,20 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     /***************************************************************/
     
     func updateSportData(json: JSON){
-        
-        
         for item in json["data"].arrayValue {
-            fixtures.awayTeam = item["visitorteam_id"].stringValue
-            fixtures.homeTeam = item["localteam_id"].stringValue
-            fixtures.date = item["time"]["starting_at"]["date"].stringValue
-            print("_____________ START _______________")
-            print(item["id"].stringValue)
-            print(item["visitorteam_id"].stringValue)
-            print(item["localteam_id"].stringValue)
-            //print(item)
-            print("_____________ END _________________")
-            fixturesData.append(fixtures)
+            let fix = GetFixtureData()
+            fix.awayTeam = item["visitorteam_id"].stringValue
+            fix.homeTeam = item["localteam_id"].stringValue
+            fix.date = item["time"]["starting_at"]["date"].stringValue
+            fixturesData.append(fix)
             self.mainTableView.reloadData()
         }
-        
     }
 
     //MARK: - TableView Settings
     /***************************************************************/
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        print("a countban")
-        print(self.fixturesData.count)
         return self.fixturesData.count
     }
     
@@ -77,6 +60,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             let fixtures = fixturesData[indexPath.row]
             cell.updateUI(fixtures: fixtures)
             return cell
+            
         }else {
             return UITableViewCell()
         }
